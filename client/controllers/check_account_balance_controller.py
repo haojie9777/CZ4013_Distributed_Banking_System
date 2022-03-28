e78cbe3752632a7160f0b866eb8e337b0b7ae9b2
WernCodes <69962697+WernCodes@users.noreply.github.com>
2022-03-29 00:56:06 +0800

    revert currency type check

--- client/controllers/check_account_balance_controller.py
+++ client/controllers/check_account_balance_controller.py
@@ -32,19 +32,14 @@ class CheckAccountBalanceController(BaseController):
         account_name = get_string_input(f'Please indicate name')
         account_number = get_int_input(f'Please indicate account number')
         account_password = get_string_input(f'Please indicate password')
-        print_options(self.options, show_number=True)
-        account_currencyType_choice = get_menu_option(max_choice=len(self.currency_list),
-                                                      msg='Please indicate account currency')
-        account_currencyType = CurrencyType[self.currency_list[account_currencyType_choice]]
-        self.handler(account_name, account_number, account_password, account_currencyType)
+        self.handler(account_name, account_number, account_password)
         print_options(self.ctrl_list)
         return get_menu_option(max_choice=len(self.ctrl_list))
 
-    def handler(self, account_name: str, account_number: int, account_password: str, account_currencyType: CurrencyType):
+    def handler(self, account_name: str, account_number: int, account_password: str):
         """
         This handles the input from the users by logging hint information and makes request to the server for
         the checking account balance
-        :param account_currencyType: currency type of client account
         :param account_number: account number of client
         :param account_name: account name of client
         :param account_password: account password of client
@@ -52,23 +47,22 @@ class CheckAccountBalanceController(BaseController):
         """
         print_message(f'Checking account balance...')
         try:
-            balance = self.retrieve_account_balance(account_name, account_number, account_password, account_currencyType)
+            balance = self.retrieve_account_balance(account_name, account_number, account_password)
             reply_string = "Account balance is: " + balance
             print_message(reply_string)
         except Exception as e:
             print_error(f"Bad request detected! {str(e)}")
 
     @staticmethod
-    def retrieve_account_balance(account_name: str, account_number: int, account_password: str, account_currencyType: CurrencyType,) -> str:
+    def retrieve_account_balance(account_name: str, account_number: int, account_password: str) -> str:
         """
         This makes request to the server for checking account balance
-        :param account_currencyType: currency type of client account
         :param account_number: account number of client
         :param account_name: account name of client
         :param account_password: account password of client
         :return: reply message from server
         """
-        reply_msg = request(ServiceType.OPEN_ACCOUNT, account_name, str(account_number), account_password, account_currencyType.value)
+        reply_msg = request(ServiceType.OPEN_ACCOUNT, account_name, str(account_number), account_password)
         if reply_msg.msg_type == MessageType.EXCEPTION:
             raise Exception(reply_msg.error_msg)
         return reply_msg.data
